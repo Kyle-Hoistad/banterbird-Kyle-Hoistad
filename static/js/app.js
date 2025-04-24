d6f5c551439f6e8a8c43f423f1c148ec1d0f0b33
Kyle-Hoistad <kyle.hoistad@ascte.org>
2025-04-24 13:55:26 -0500

    updated code as per 6-2-3 and added the start to login page
    My code for some reason likes to put the new post at the bottom until I refresh the page, I checked it against other classmate's code and mine is the same. as a temporary fix I have location.reload at the bottom to make sure the new post goes to the top.

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,30 @@
 const username = "Jimmy Johnson";
 
-function renderPost(post) {
+function renderPost(post, isNew = false) {
     const template = document.getElementById("post-template").content.cloneNode(true);
     template.querySelector(".username").innerText = post.username;
     template.querySelector(".message").innerText = post.message;
     document.getElementById("feed").appendChild(template);
+
+    if (isNew) {
+        document.getElementById("feed").prepend(template);
+    } else {
+        document.getElementById("feed").appendChild(template);
+    }
 }
 
-function submitPost() {
+async function submitPost() {
     const message = document.getElementById("postInput").value;
     try {
-        const response = fetch("/api/banter", {
+        const response = await fetch("/api/banter", {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                username: username,
-                message: message,
-            }),
+            headers: {"Content-Type": "application/json",},
+            body: JSON.stringify({username, message,}),
         });
-
+        if (response.ok) {
+            renderPost({ username, message }, true);
+            document.getElementById("postInput").value = "";
+        }
     } catch (error) {
         console.error("Post failed ☠️☠️☠️ ☆*: .｡. o(≧▽≦)o .｡.:*☆ o(≧口≦)o o((>ω< ))o ヽ（≧□≦）ノ ☠️☠️☠️ ╰(*°▽°*)╯ ╰(*°▽°*)╯", error)
     }
@@ -31,9 +35,7 @@ window.onload = async () => {
     try {
         const response = await fetch("/api/posts");
         const posts = await response.json();
-        posts.forEach((posts) => {
-            renderPost(posts);
-        });
+        posts.forEach((posts) => renderPost(posts));
     } catch (error) { 
         console.error("FIXXX ITTT", error); 
     }
